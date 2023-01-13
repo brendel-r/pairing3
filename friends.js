6edbde41f6b68638a0f82fbe690022756a95bc3f
Rachel Brendel <brendelrachel@gmail.com>
2023-01-12 20:50:16 -0600

    Add finial change

--- friends.js
+++ friends.js
@@ -1,6 +1,11 @@
 var numbers = [1, 8, 17, 25, 69, 72, 98, 14];
+// for (var i = 0; i < 8; i++) {
+  // if (numbers[i] > 30) {
+    // console.log(numbers[i]); 
+  // }
+// }
 for (var i = 0; i < 8; i++) {
-  if (numbers[i] > 30) {
-    console.log(numbers[i]); 
+  if (numbers[i] < 60) { 
+    console.log(numbers[i]);
   }
 }

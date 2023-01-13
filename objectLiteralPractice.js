72e18d05863a90c68b8bd52d616777872fb0705f
Rachel Brendel <brendelrachel@gmail.com>
2023-01-12 19:12:38 -0600

    Initial commit

---
+++ objectLiteralPractice.js
@@ -0,0 +1,12 @@
+
+var person = {
+  fullName: "Rachel Brendel",
+  Age: 33,
+  married: true,
+  hasKids: false
+}
+console.log(person)
+
+person.height = "5'7";
+person.hairColor = "Blonde"
+console.log(person)
